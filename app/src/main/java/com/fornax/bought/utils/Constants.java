b637b8ed9b23f6a82b976f9f942942bd7c9d8132
fabianoyoschitaki <fabianoyoschitaki@gmail.com>
2016-01-13 14:17:53 -0200

    Update Constants.java

--- app/src/main/java/com/fornax/bought/utils/Constants.java
+++ app/src/main/java/com/fornax/bought/utils/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
     public static final String REST_BASE_BOUGHT_URL = "http://54.200.195.122/bought-web";
 
     public static final String REST_TODOS_PRODUTOS_URL = "/produtos/todos";
-    public static final String REST_OBTER_PRODUTO_URL = "/produtos/obterproduto/{codigoBarras}";
+    public static final String REST_OBTER_PRODUTO_URL = "/produtos/obter/{codigoBarras}";
     public static final String REST_AUTENTICAR_URL = "/login/autenticar/{email}/{senha}";
 
     public static final String REST_TODOS_MERCADOS = "/mercados/todos";
